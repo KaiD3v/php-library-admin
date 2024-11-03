9ca5f94f28f079fee0f094968f75e782fe5600bf
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-11-03 13:54:51 -0300

    list book cards

--- actions/book-actions.php
+++ actions/book-actions.php
@@ -6,16 +6,16 @@ switch ($_REQUEST["action"]) {
         $description = $_POST['description'];
         $coverImg = $_POST['cover_img'];
         $quantity = $_POST['quantity'];
-    
+
         $sql = "INSERT INTO books (title, description, cover_img, quantity) VALUES (:title, :description, :coverImg, :quantity)";
-    
+
         $stmt = $pdo->prepare($sql);
-    
+
         $stmt->bindParam(':title', $title);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':coverImg', $coverImg);
         $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
-    
+
         if ($stmt->execute()) {
             echo "<script>alert('Livro cadastrado com sucesso.')</script>";
             echo "<script>location.href='?page=books'</script>";
--- pages/books.php
+++ pages/books.php
@@ -15,3 +15,49 @@ if (isset($_SESSION['email'])) {
     ";
 }
 ?>
+
+<div class="row g-4"> <!-- Gutter para espaçamento entre colunas e linhas -->
+    <?php
+    $sql = "SELECT * FROM books";
+    $res = $pdo->query($sql);
+    $qtd = $res->rowCount();
+
+    if ($qtd > 0 and isset($_SESSION['email'])) {
+        while ($row = $res->fetchObject()) {
+            echo "
+            <div class='col-12 col-sm-6 col-lg-4'> <!-- Responsivo: 1 por linha em mobile, 2 em tablets, 3 em desktops -->
+                <div class='card h-100 shadow-sm'>
+                    <img src='{$row->cover_img}' class='card-img-top' alt='Imagem de capa do livro' style='height: 300px; object-fit: cover;'>
+                    <div class='card-body d-flex flex-column'>
+                        <h5 class='card-title'>{$row->title}</h5>
+                        <p class='card-text flex-grow-1'>{$row->description}</p>
+                        <p><b>Quantidade:</b> {$row->quantity}</p>
+                        <div class='mt-3'>
+                            <a href='?page=edit-book&id={$row->id}' class='btn btn-warning'>Editar</a>
+                            <a href='?page=rent-book&id={$row->id}' class='btn btn-success'>Alugar</a>
+                            <a href='?page=delete-book&id={$row->id}' class='btn btn-danger' onclick='return confirm(\"Você tem certeza que deseja deletar este livro?\")'>Deletar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+        }
+    } else if ($qtd > 0 and $_SESSION['email'] === null) {
+        while ($row = $res->fetchObject()) {
+            echo "
+                <div class='col-12 col-sm-6 col-lg-4'> <!-- Responsivo: 1 por linha em mobile, 2 em tablets, 3 em desktops -->
+                    <div class='card h-100 shadow-sm'>
+                        <img src='{$row->cover_img}' class='card-img-top' alt='Imagem de capa do livro' style='height: 300px; object-fit: cover;'>
+                        <div class='card-body d-flex flex-column'>
+                            <h5 class='card-title'>{$row->title}</h5>
+                            <p class='card-text flex-grow-1'>{$row->description}</p>
+                            <p><b>Quantidade:</b> {$row->quantity}</p>
+                            
+                        </div>
+                    </div>
+                </div>";
+        }
+    } else {
+        echo "<p class='alert alert-danger'>Não há nenhum livro disponível.</p>";
+    }
+    ?>
+</div>
