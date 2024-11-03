0f8cde02e824eae5f2b6ac8d5ca80c7e155a0c9f
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-11-03 17:28:20 -0300

    add home page

--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,45 @@
-<h1>Home Page</h1>
-<?php
-if (isset($_SESSION['email'])) {
-    echo "Bem-vindo, " . $_SESSION['email'];
-} else {
-    echo "Você não está logado.";
-}
-?>
+<header class="bg-primary text-white text-center py-5">
+    <div class="container">
+        <h1 class="display-4">Bem-vindo ao Gerenciamento de Biblioteca</h1>
+        <p class="lead">Este é um projeto desenvolvido para estudar PHP com gerenciamento de livros.</p>
+    </div>
+</header>
+
+<div class="container my-5">
+    <h2 class="text-center">Imagens do Projeto</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src="../public/assets/books.png" class="card-img-top" alt="Descrição da imagem 1">
+                <div class="card-body">
+                    <h5 class="card-title">Página de livros</h5>
+                    <p class="card-text">Essa é a página de livros da visão de um administrador autenticado, fornecendo opções de aluguel, edição e deleção de livro.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src="../public/assets/users.png" class="card-img-top" alt="Descrição da imagem 2">
+                <div class="card-body">
+                    <h5 class="card-title">Página de gerenciamento de clientes</h5>
+                    <p class="card-text">Essa é a página de gerenciamento de clientes, acessível somente por administradores autenticados.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src="../public/assets/new-book.png" class="card-img-top" alt="Descrição da imagem 3">
+                <div class="card-body">
+                    <h5 class="card-title">Formulário de controle de livros</h5>
+                    <p class="card-text">Essa é a página de cadastro de livros, acessível somente por administradores.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="text-center py-4 bg-light">
+    <div class="container">
+        <p>&copy; 2024 Sistema de Gerenciamento de Biblioteca. Todos os direitos reservados.</p>
+    </div>
+</footer>
