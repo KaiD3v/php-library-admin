7f31d56006e0c5007bfe81916c926e65e0c1298e
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-11-03 14:15:46 -0300

    add rent modal bootstrap

--- pages/books.php
+++ pages/books.php
@@ -16,6 +16,25 @@ if (isset($_SESSION['email'])) {
 }
 ?>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Alugar Livro</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Você está prestes a alugar um livro. Confirme sua ação.</p>
+                <!-- Aqui você pode adicionar mais detalhes sobre o livro que está sendo alugado -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Confirmar Aluguel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="row g-4"> <!-- Gutter para espaçamento entre colunas e linhas -->
     <?php
     $sql = "SELECT * FROM books";
@@ -25,7 +44,7 @@ if (isset($_SESSION['email'])) {
     if ($qtd > 0 and isset($_SESSION['email'])) {
         while ($row = $res->fetchObject()) {
             echo "
-            <div class='col-12 col-sm-6 col-lg-4'> <!-- Responsivo: 1 por linha em mobile, 2 em tablets, 3 em desktops -->
+            <div class='col-12 col-sm-6 col-lg-4'>
                 <div class='card h-100 shadow-sm'>
                     <img src='{$row->cover_img}' class='card-img-top' alt='Imagem de capa do livro' style='height: 300px; object-fit: cover;'>
                     <div class='card-body d-flex flex-column'>
@@ -34,7 +53,7 @@ if (isset($_SESSION['email'])) {
                         <p><b>Quantidade:</b> {$row->quantity}</p>
                         <div class='mt-3'>
                             <a href='?page=edit-book&id={$row->id}' class='btn btn-warning'>Editar</a>
-                            <a href='?page=rent-book&id={$row->id}' class='btn btn-success'>Alugar</a>
+                            <button data-bs-toggle='modal' data-bs-target='#exampleModal' class='btn btn-success'>Alugar</button>
                             <a href='?page=delete-book&id={$row->id}' class='btn btn-danger' onclick='return confirm(\"Você tem certeza que deseja deletar este livro?\")'>Deletar</a>
                         </div>
                     </div>
