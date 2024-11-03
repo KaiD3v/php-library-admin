4cb89100f74602e81631e47a0dafcd6cc91cc45c
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-11-02 23:22:33 -0300

    list admins

--- pages/admins.php
+++ pages/admins.php
@@ -4,3 +4,35 @@ if ($_SESSION['email'] == null) {
 }
 ?>
 <h1>Admin Page</h1>
+<table class='table table-hover'>
+    <?php
+    $sql = "SELECT * FROM admins";
+
+    $res = $pdo->query($sql);
+
+    $qtd = $res->rowCount();
+
+    if ($qtd > 0) {
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th>#</th>";
+        echo "<th>username</th>";
+        echo "<th>email</th>";
+        echo "</tr>";
+        echo "</thead>";
+
+        echo "<tbody>";
+        while ($row = $res->fetchObject()) {
+            echo "<tr>";
+            echo "<td>$row->id</td>";
+            echo "<td>$row->username</td>";
+            echo "<td>$row->email</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+    } else {
+        echo "<p class='alert alert-danger'>Ainda não há nenhum administador cadastrado.</P>";
+    }
+
+    ?>
+</table>
